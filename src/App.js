69727102d4d9fccf66dee5c891422f41bd9a4ed5
ZakYeo <zakyeomanson@googlemail.com>
2024-06-23 14:21:22 +0100

    Added animation + updated styling

--- src/App.js
+++ src/App.js
@@ -18,28 +18,28 @@ function App() {
   }, []);
 
   return (
-      <div className="relative flex items-center justify-center min-h-screen overflow-hidden">
-        <video
-          autoPlay
-          loop
-          muted
-          className="absolute w-auto min-w-full min-h-full max-w-none"
-        >
-          <source src={`${process.env.PUBLIC_URL}/bg-clouds.mp4`} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-        <div className="relative bg-white bg-opacity-70 p-10 rounded-lg shadow-lg text-center">
-          <h1 className="text-4xl font-bold mb-4 text-blue-800">Should you wear a coat today?</h1>
-          {shouldWearCoat !== null ? (
-            <p className={`text-2xl ${shouldWearCoat ? 'text-green-500' : 'text-red-500'}`}>
-              {shouldWearCoat ? 'Yes, you should wear a coat!' : 'No, you don\'t need a coat!'}
-            </p>
-          ) : (
-            <p className="text-2xl text-gray-700">Loading...</p>
-          )}
-        </div>
+    <div className="relative flex items-center justify-center min-h-screen overflow-hidden">
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute w-auto min-w-full min-h-full max-w-none filter blur-md"
+      >
+        <source src={`${process.env.PUBLIC_URL}/bg-clouds.mp4`} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="relative bg-white bg-opacity-70 p-10 rounded-lg shadow-lg text-center animate-fadeIn">
+        <h1 className="text-4xl font-bold mb-4 text-black drop-shadow-lg">Should you wear a coat today?</h1>
+        {shouldWearCoat !== null ? (
+          <p className={`text-2xl text-black drop-shadow-lg ${shouldWearCoat ? 'text-green-500' : 'text-red-500'}`}>
+            {shouldWearCoat ? 'Yes, you should wear a coat!' : 'No, you don\'t need a coat!'}
+          </p>
+        ) : (
+          <p className="text-2xl text-gray-200 drop-shadow-lg">Loading...</p>
+        )}
       </div>
-    );
+    </div>
+  );
 }
 
 export default App;
