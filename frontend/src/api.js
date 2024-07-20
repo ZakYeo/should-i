14acdedd29354d8cd0f3b245bb1932bfc7bae437
ZakYeo <zakyeomanson@googlemail.com>
2024-07-20 15:22:38 +0100

    Configured backend for CORS to support API calls from frontend ReactJS web application via port 3001

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,12 @@
 import axios from "axios"
 
-const API_URL = "http://localhost:5000/api/"
+const API_URL = process.env.REACT_APP_API_URL
 
 export const checkCoat = async (latitude, longitude) => {
   try {
+    console.log(`${API_URL}check-coat?lat=${latitude}&lon=${longitude}`)
+    console.log(`${API_URL}check-coat?lat=${latitude}&lon=${longitude}`)
+    console.log(`${API_URL}check-coat?lat=${latitude}&lon=${longitude}`)
     const response = await axios.get(
       `${API_URL}check-coat?lat=${latitude}&lon=${longitude}`,
     )
