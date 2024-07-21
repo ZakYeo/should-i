b90d86223526d4eb8e88ccdca48a8c6fd7ea95f8
ZakYeo <zakyeomanson@googlemail.com>
2024-07-21 13:35:38 +0100

    Updated save-comment-to-db lambda to save an incoming comment to DynamoDB

--- backend/lambda-functions/save-comment-to-db/src/handlers/save-comment-to-db.mjs
+++ backend/lambda-functions/save-comment-to-db/src/handlers/save-comment-to-db.mjs
@@ -1,12 +1,46 @@
-/**
- * A Lambda function that returns a static string
- */
-export const handler = async () => {
-  // If you change this message, you will need to change hello-from-lambda.test.mjs
-  const message = 'Hello from Lambda!';
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+import geohash from 'ngeohash';
 
-  // All log statements are written to CloudWatch
-  console.info(`${message}`);
 
-  return message;
+const isLocal = true;
+// command sam local start-api starts aws resources in a docker container, so use this endpoint
+// So lambda knows to point to localhost of the machine, as localhost:8000 will point to localhost of the docker
+const localEndpoint = "http://host.docker.internal:8000";
+
+const client = new DynamoDBClient({
+  region: 'eu-west-2', endpoint: isLocal ? localEndpoint : undefined
+});
+const ddbDocClient = DynamoDBDocumentClient.from(client);
+const precision = 6;
+
+
+export async function handler(event) {
+  const { userName, commentDescription, thumbsUpAmount, thumbsDownAmount, latitude, longitude } = event;
+
+  const commentId = Date.now().toString();
+
+  const geoHash = geohash.encode(latitude, longitude, precision);
+
+  const putParams = {
+    TableName: 'Comments',
+    Item: {
+      CommentId: commentId,
+      UserName: userName,
+      Description: commentDescription,
+      ThumbsUp: thumbsUpAmount,
+      ThumbsDown: thumbsDownAmount,
+      Latitude: latitude,
+      Longitude: longitude,
+      Geohash: geoHash
+    }
+  };
+
+  try {
+    await ddbDocClient.send(new PutCommand(putParams));
+    return { statusCode: 200, body: JSON.stringify({ message: 'Comment added successfully!', commentId }) };
+  } catch (error) {
+    console.error('Error adding comment:', error);
+    return { statusCode: 500, body: JSON.stringify({ message: 'Failed to add comment', errorMessage: error.message }) };
+  }
 }
