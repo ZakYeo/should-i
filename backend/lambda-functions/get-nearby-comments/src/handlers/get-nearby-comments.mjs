d7806e6990e878eb28f9bdc0e8a389fb80c266ff
ZakYeo <zakyeomanson@googlemail.com>
2024-07-22 07:32:40 +0100

    Added latitude / longitude validation for get-nearby-comments lambda

--- backend/lambda-functions/get-nearby-comments/src/handlers/get-nearby-comments.mjs
+++ backend/lambda-functions/get-nearby-comments/src/handlers/get-nearby-comments.mjs
@@ -6,6 +6,10 @@ const isLocal = true;
 // command sam local start-api starts aws resources in a docker container, so use this endpoint
 // So lambda knows to point to localhost of the machine, as localhost:8000 will point to localhost of the docker
 const localEndpoint = "http://host.docker.internal:8000";
+const LATITUDE_MINIMUM = -90;
+const LATITUDE_MAXIMUM = 90;
+const LONGITUDE_MINIMUM = -180;
+const LONGITUDE_MAXIMUM = 180;
 
 const client = new DynamoDBClient({
   region: 'eu-west-2', endpoint: isLocal ? localEndpoint : undefined
@@ -14,6 +18,42 @@ const ddbDocClient = DynamoDBDocumentClient.from(client);
 
 export const handler = async (event) => {
   const { lat, lon } = event.queryStringParameters;
+
+  if (!lat || !lon) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Missing latitude or longitude parameter' }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+  }
+
+  let latitude = parseFloat(lat);
+  let longitude = parseFloat(lon);
+  if (isNaN(latitude) || isNaN(longitude)) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Latitude and longitude must be valid numbers' }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+  }
+
+  latitude = Number(latitude.toFixed(6));
+  longitude = Number(longitude.toFixed(6));
+
+  if (latitude < LATITUDE_MINIMUM || latitude > LATITUDE_MAXIMUM ||
+    longitude < LONGITUDE_MINIMUM || longitude > LONGITUDE_MAXIMUM) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Latitude must be between -90 and 90, and longitude must be between -180 and 180' }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+  }
 
 
   const geoHash = geohash.encode(lat, lon, 6);
