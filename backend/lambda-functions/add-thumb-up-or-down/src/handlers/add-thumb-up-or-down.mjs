41f87fcc80d6dcdb8a799b39af2b44e324a430c1
ZakYeo <zakyeomanson@googlemail.com>
2024-07-31 18:25:29 +0100

    Added validation to add-thumb-up-or-down lambda -> missing commentId, invalid voteType & missing commentId

--- backend/lambda-functions/add-thumb-up-or-down/src/handlers/add-thumb-up-or-down.mjs
+++ backend/lambda-functions/add-thumb-up-or-down/src/handlers/add-thumb-up-or-down.mjs
@@ -1,5 +1,5 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, UpdateCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, UpdateCommand, GetCommand } from "@aws-sdk/lib-dynamodb";
 
 const isLocal = true;
 // command sam local start-api starts aws resources in a docker container, so use this endpoint
@@ -13,6 +13,53 @@ const ddbDocClient = DynamoDBDocumentClient.from(client);
 
 export async function handler(event) {
   const { commentId, voteType } = JSON.parse(event.body);
+
+  if (!voteType || (voteType !== 'up' && voteType !== 'down')) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Invalid vote type. Must be "thumbsUp" or "thumbsDown"' }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+  }
+
+  if (!commentId) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'commentId cannot be empty or null' }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+  }
+
+  const getParams = {
+    TableName: 'Comments',
+    Key: { CommentId: commentId }
+  };
+
+  try {
+    const { Item } = await ddbDocClient.send(new GetCommand(getParams));
+    if (!Item) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: 'Comment not found' }),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      };
+    }
+  } catch (error) {
+    console.error('Error retrieving comment:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Failed to retrieve comment', errorMessage: error.message }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+  }
 
   const updateParams = {
     TableName: 'Comments',
