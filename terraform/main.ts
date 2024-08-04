2f545c15a6bbd443e06fb8428ffe5b85f5013b55
ZakYeo <zakyeomanson@googlemail.com>
2024-08-04 15:50:48 +0100

    Terraform now deploys all relevant lambdas for should-i project

--- terraform/main.ts
+++ terraform/main.ts
@@ -27,9 +27,21 @@ class MyStack extends TerraformStack {
       region: "eu-west-2",
     });
     // Create lambda executable
-    const asset = new TerraformAsset(this, "lambda-asset-save-comment-to-db", {
+    const assetSaveCommentToDB = new TerraformAsset(this, "lambda-asset-save-comment-to-db", {
       path: '../backend/lambda-functions/save-comment-to-db/',
-      type: AssetType.ARCHIVE, // if left empty it infers directory and file
+      type: AssetType.ARCHIVE,
+    });
+    const assetGetNearbyComments = new TerraformAsset(this, "lambda-asset-get-nearby-comments", {
+      path: '../backend/lambda-functions/get-nearby-comments/',
+      type: AssetType.ARCHIVE,
+    });
+    const assetCheckCoat = new TerraformAsset(this, "lambda-asset-check-coat", {
+      path: '../backend/lambda-functions/check-coat/',
+      type: AssetType.ARCHIVE,
+    });
+    const assetAddThumbUpOrThumbDown = new TerraformAsset(this, "lambda-asset-add-thumb-up-or-down", {
+      path: '../backend/lambda-functions/add-thumb-up-or-down/',
+      type: AssetType.ARCHIVE,
     });
 
     // Create bucket to store lambdas
@@ -39,10 +51,25 @@ class MyStack extends TerraformStack {
     });
 
     // Upload lambda zip file to lambda store on s3
-    new aws.s3Object.S3Object(this, "lambda-archive", {
+    new aws.s3Object.S3Object(this, "lambda-archive-save-comment-to-db", {
       bucket: lambdaBucket.bucket,
-      key: `save-comment-to-db/${asset.fileName}`,
-      source: asset.path,
+      key: `save-comment-to-db/${assetSaveCommentToDB.fileName}`,
+      source: assetSaveCommentToDB.path,
+    });
+    new aws.s3Object.S3Object(this, "lambda-archive-get-nearby-comments", {
+      bucket: lambdaBucket.bucket,
+      key: `get-nearby-comments/${assetGetNearbyComments.fileName}`,
+      source: assetGetNearbyComments.path,
+    });
+    new aws.s3Object.S3Object(this, "lambda-archive-check-coat", {
+      bucket: lambdaBucket.bucket,
+      key: `check-coat/${assetCheckCoat.fileName}`,
+      source: assetCheckCoat.path,
+    });
+    new aws.s3Object.S3Object(this, "lambda-archive-add-thumb-up-or-down", {
+      bucket: lambdaBucket.bucket,
+      key: `add-thumb-up-or-down/${assetAddThumbUpOrThumbDown.fileName}`,
+      source: assetAddThumbUpOrThumbDown.path,
     });
 
     // Create Lambda role
@@ -64,8 +91,44 @@ class MyStack extends TerraformStack {
       {
         functionName: `save-comment-to-db`,
         s3Bucket: lambdaBucket.bucket,
-        s3Key: `save-comment-to-db/${asset.fileName}`,
+        s3Key: `save-comment-to-db/${assetSaveCommentToDB.fileName}`,
         handler: `src/handlers/save-comment-to-db.handler`,
+        runtime: `nodejs18.x`,
+        role: role.arn
+      }
+    );
+    new aws.lambdaFunction.LambdaFunction(
+      this,
+      `lambda-get-nearby-comments`,
+      {
+        functionName: `get-nearby-comments`,
+        s3Bucket: lambdaBucket.bucket,
+        s3Key: `get-nearby-comments/${assetGetNearbyComments.fileName}`,
+        handler: `src/handlers/get-nearby-comments.handler`,
+        runtime: `nodejs18.x`,
+        role: role.arn
+      }
+    );
+    new aws.lambdaFunction.LambdaFunction(
+      this,
+      `lambda-check-coat`,
+      {
+        functionName: `check-coat`,
+        s3Bucket: lambdaBucket.bucket,
+        s3Key: `check-coat/${assetCheckCoat.fileName}`,
+        handler: `src/handlers/check-coat.handler`,
+        runtime: `nodejs18.x`,
+        role: role.arn
+      }
+    );
+    new aws.lambdaFunction.LambdaFunction(
+      this,
+      `lambda-add-thumb-up-or-down`,
+      {
+        functionName: `add-thumb-up-or-down`,
+        s3Bucket: lambdaBucket.bucket,
+        s3Key: `add-thumb-up-or-down/${assetAddThumbUpOrThumbDown.fileName}`,
+        handler: `src/handlers/add-thumb-up-or-down.handler`,
         runtime: `nodejs18.x`,
         role: role.arn
       }
